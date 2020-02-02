c5bbdca2cd01a5494d386eebcf48713a6b028cd1
Saurabh Jha <59443058+saurabhjha443@users.noreply.github.com>
2020-02-02 16:53:47 +0530

    Update loan data.py

--- Loan Prediction/loan data.py
+++ Loan Prediction/loan data.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import classification_report,accuracy_score
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 #read csv file in daatFrame
